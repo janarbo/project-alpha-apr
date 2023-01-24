958b8a4cb07e91848d4b6aabe42efa19f2eaf89f
Janar Bo <janarboke@gmail.com>
2023-01-23 20:16:53 -0800

    Feature 13 complete

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
 
@@ -10,3 +10,13 @@ def list_projects(request):
         "projects": projects,
     }
     return render(request, "project/projects_list.html", context)
+
+
+@login_required
+def show_project(request, id):
+    detail = Project.objects.get(id=id)
+    context = {
+        "project_object": detail,
+    
+    }
+    return render(request, "project/detail.html", context)
