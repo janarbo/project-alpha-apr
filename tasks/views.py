a3a2cd2ce56522ad25d7336719769d8d04c74daf
Janar Bo <janarboke@gmail.com>
2023-01-25 09:29:24 -0800

    Final submission

--- tasks/views.py
+++ tasks/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from tasks.forms import TaskForm
 from django.contrib.auth.decorators import login_required
 from tasks.models import Task
-from django.views.generic import ListView
+
 
 @login_required
 def create_task(request):
